0145598ec04ab643634b12234b7ea7d5ae8f27d9
Baek DongJae <pandora2948@naver.com>
2022-10-14 16:41:42 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1ad85f3336b9ee653753.js"
+    "url": "webpack-runtime-c09c6f2787eddbd62a90.js"
   },
   {
     "url": "styles.c75940ba2e9d08c2bb15.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "137756726b6804203fc61a1f73b9a73c"
+    "revision": "fb62e9b9312b7b753f12a472db5e2fad"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
